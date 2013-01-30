3c429f67e96b824a5fe8a6d2163d8905b7e23c6e
Ben Greear <greearb@candelatech.com>
2013-01-29 22:48:27 -0800

    lockd: Support binding nlm client to specific address.
    This ensures that file locking messages use the proper
    source IP address if the file system has been mounted
    with a specified source IP address.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- fs/lockd/lockd.h
+++ fs/lockd/lockd.h
@@ -275,6 +275,7 @@ extern const struct rpc_version nlm_version4;
  * Host cache
  */
 struct nlm_host  *nlmclnt_lookup_host(const struct sockaddr *sap,
+					const struct sockaddr *bindaddr,
 					const size_t salen,
 					const unsigned short protocol,
 					const u32 version,
