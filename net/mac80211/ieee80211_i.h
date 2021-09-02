badbed96b62780e6a7b9dcf513903612cd5d01a2
Ben Greear <greearb@candelatech.com>
2021-09-02 16:55:59 -0700

    mac80211/mt76: Allow disabling OFDMA (non upstream)
    This lets us disable OFDMA even if peer supports this.
    It did not actually solve the RF interference problem, but
    maybe useful for other things.
    
    Too much of a hack to go upstream most likely.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/mac80211/ieee80211_i.h
+++ net/mac80211/ieee80211_i.h
@@ -390,33 +390,6 @@ enum ieee80211_sta_flags {
 	IEEE80211_STA_ENABLE_RRM	= BIT(15),
 };
 
-enum ieee80211_conn_mode {
-	IEEE80211_CONN_MODE_S1G,
-	IEEE80211_CONN_MODE_LEGACY,
-	IEEE80211_CONN_MODE_HT,
-	IEEE80211_CONN_MODE_VHT,
-	IEEE80211_CONN_MODE_HE,
-	IEEE80211_CONN_MODE_EHT,
-	IEEE80211_CONN_MODE_UHR,
-};
-
-#define IEEE80211_CONN_MODE_HIGHEST	IEEE80211_CONN_MODE_UHR
-
-enum ieee80211_conn_bw_limit {
-	IEEE80211_CONN_BW_LIMIT_20,
-	IEEE80211_CONN_BW_LIMIT_40,
-	IEEE80211_CONN_BW_LIMIT_80,
-	IEEE80211_CONN_BW_LIMIT_160, /* also 80+80 */
-	IEEE80211_CONN_BW_LIMIT_320,
-};
-
-struct ieee80211_conn_settings {
-	enum ieee80211_conn_mode mode;
-	enum ieee80211_conn_bw_limit bw_limit;
-	enum ieee80211_conn_flags conn_flags;
-	bool dbe_enabled;
-};
-
 extern const struct ieee80211_conn_settings ieee80211_conn_settings_unlimited;
 
 struct ieee80211_mgd_auth_data {
