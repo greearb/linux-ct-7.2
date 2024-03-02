0fe2828b897c3ea8528be016a025479d6bf147bd
Ben Greear <greearb@candelatech.com>
2024-03-01 16:57:18 -0800

    mediatek: add option to read rf data from a firmware file
    Signed-off-by: Scott Iveland <scott.iveland@smartrg.com>
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/eeprom.c
+++ drivers/net/wireless/mediatek/mt76/eeprom.c
@@ -152,7 +152,7 @@ exit:
 }
 EXPORT_SYMBOL_GPL(mt76_get_of_data_from_nvmem);
 
-static int mt76_get_of_data_from_file(struct mt76_dev *dev, void *eep, u32 offset, int len)
+int mt76_get_of_data_from_file(struct mt76_dev *dev, void *eep, u32 offset, int len)
 {
 #if defined(CONFIG_OF)
 	int ret = 0;
@@ -220,6 +220,7 @@ out_put_node:
 	return -ENOENT;
 #endif
 }
+EXPORT_SYMBOL_GPL(mt76_get_of_data_from_file);
 
 static int mt76_get_of_eeprom(struct mt76_dev *dev, void *eep, int len)
 {
@@ -617,8 +618,8 @@ int
 mt76_eeprom_init(struct mt76_dev *dev, int len)
 {
 #if defined(CONFIG_OF)
-        u32 offset;
-        struct device_node *np = dev->dev->of_node;
+	u32 offset;
+	struct device_node *np = dev->dev->of_node;
 #endif
 
 	dev->eeprom.size = len;
