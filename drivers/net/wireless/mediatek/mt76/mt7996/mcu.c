234e603e72571328ae6289052077e9c2d5e565d7
Ben Greear <greearb@candelatech.com>
2024-10-09 14:32:26 -0700

    mtk7996: Do not decrease OFDM or CCK rates by NSS
    Since they are always transmitted at single NSS anyway.
    
    For txpower set to '10':
    
    [root@204-tr398-mgr ~]# cat /debug/ieee80211/wiphy0/mt76/txpower_sku
    
    Phy 0 TX Power Table (Channel 6)
                               1m     2m     5m    11m
    CCK (TMAC)           :     20     20     20     20
                               6m     9m    12m    18m    24m    36m    48m    54m
    OFDM (TMAC)          :     20     20     20     20     20     20     20     20
                             mcs0   mcs1   mcs2   mcs3   mcs4   mcs5   mcs6   mcs7
    HT20 (TMAC)          :      8      8      8      8      8      8      8      8
    ...
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7996/mcu.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/mcu.c
@@ -5546,6 +5546,7 @@ int mt7996_mcu_set_txpower_sku(struct mt7996_phy *phy)
 	struct mt76_power_path_limits la_path = {};
 	struct sk_buff *skb;
 	int i, ret, txpower_limit;
+	s8 single_nss_txpower;
 
 	if (phy->txpower == INT_MIN || phy->txpower > 127)
 		phy->txpower = 127;
@@ -5555,7 +5556,7 @@ int mt7996_mcu_set_txpower_sku(struct mt7996_phy *phy)
 	else
 		txpower_limit = phy->txpower;
 
-	txpower_limit = mt76_get_power_bound(mphy, txpower_limit);
+	txpower_limit = mt7996_get_power_bound(mphy, txpower_limit, &single_nss_txpower);
 
 	if (phy->sku_limit_en) {
 		txpower_limit = mt76_get_rate_power_limits(mphy, mphy->chandef.chan,
@@ -5619,6 +5620,10 @@ int mt7996_mcu_set_txpower_sku(struct mt7996_phy *phy)
 		if (!phy->default_txpower)
 			return 0; /* try again in a bit */
 	}
+
+	// CCK and OFDM should use single NSS power
+	memset(&la.cck, single_nss_txpower, sizeof(la.cck));
+	memset(&la.ofdm, single_nss_txpower, sizeof(la.ofdm));
 
 	skb = mt76_mcu_msg_alloc(&dev->mt76, NULL,
 				 sizeof(req) + MT7996_SKU_PATH_NUM);
