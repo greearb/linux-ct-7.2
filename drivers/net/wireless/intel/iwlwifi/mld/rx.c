982afdc3d4281c2a669f9750b3e68a94e9881932
Ben Greear <greearb@candelatech.com>
2025-04-14 15:56:22 -0700

    wifi: iwlwifi: mld: Use driver rssi stats to determine link RSSI
    The beacon stats appear to be buggy for the secondary link
    and/or maybe just because I'm disabling beacon filtering.
    So calculate these in the driver instead, similar to how I
    did per-vif averages in the past.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/intel/iwlwifi/mld/rx.c
+++ drivers/net/wireless/intel/iwlwifi/mld/rx.c
@@ -185,6 +185,7 @@ static void iwl_mld_fill_signal(struct iwl_mld *mld, int link_id,
 				struct ieee80211_rx_status *rx_status,
 				struct iwl_mld_rx_phy_data *phy_data,
 				struct ieee80211_sta *sta,
+				struct ieee80211_link_sta *link_sta,
 				bool is_beacon, bool my_beacon)
 {
 	u32 rate_n_flags = phy_data->rate_n_flags;
@@ -192,6 +193,7 @@ static void iwl_mld_fill_signal(struct iwl_mld *mld, int link_id,
 	int energy_b = phy_data->energy_b;
 	int max_energy;
 	struct iwl_mld_sta *mld_sta = NULL;
+	struct iwl_mld_link *mld_link = NULL;
 
 	if (sta && !(is_beacon && !my_beacon)) {
 		mld_sta = iwl_mld_sta_from_mac80211(sta);
@@ -234,11 +236,22 @@ static void iwl_mld_fill_signal(struct iwl_mld *mld, int link_id,
 	rx_status->chain_signal[1] = energy_b;
 
 	if (mld_sta) {
+		if (link_sta) {
+			struct iwl_mld_vif *mld_vif;
+
+			mld_vif = iwl_mld_vif_from_mac80211(mld_sta->vif);
+			mld_link = mld_vif->link[link_sta->link_id];
+		}
 		if (is_beacon) {
-			if (my_beacon)
+			if (my_beacon) {
 				ewma_signal_add(&mld_sta->rx_avg_beacon_signal, -max_energy);
+				if (mld_link)
+					ewma_signal_add(&mld_link->rx_avg_beacon_signal, -max_energy);
+			}
 		} else {
 			ewma_signal_add(&mld_sta->rx_avg_signal, -max_energy);
+			if (mld_link)
+				ewma_signal_add(&mld_link->rx_avg_signal, -max_energy);
 		}
 	}
 }
@@ -1553,7 +1566,8 @@ static void iwl_mld_rx_fill_status(struct iwl_mld *mld, int link_id,
 				   struct ieee80211_hdr *hdr,
 				   struct sk_buff *skb,
 				   struct iwl_mld_rx_phy_data *phy_data,
-				   struct ieee80211_sta *sta)
+				   struct ieee80211_sta *sta,
+				   struct ieee80211_link_sta *link_sta)
 {
 	struct ieee80211_rx_status *rx_status = IEEE80211_SKB_RXCB(skb);
 	u32 rate_n_flags = phy_data->rate_n_flags;
@@ -1565,8 +1579,12 @@ static void iwl_mld_rx_fill_status(struct iwl_mld *mld, int link_id,
 	is_beacon = hdr && (ieee80211_is_beacon(hdr->frame_control));
 	if (is_beacon && sta) {
 		/* see if it is beacon destined for us */
-		if (memcmp(sta->addr, hdr->addr2, ETH_ALEN) == 0)
+		if (memcmp(sta->addr, hdr->addr2, ETH_ALEN) == 0) {
 			my_beacon = true;
+		} else if (link_sta) {
+			if (memcmp(link_sta->addr, hdr->addr2, ETH_ALEN) == 0)
+				my_beacon = true;
+		}
 	}
 
 	rx_status->device_timestamp = phy_data->gp2_on_air_rise;
@@ -1634,7 +1652,8 @@ static void iwl_mld_rx_fill_status(struct iwl_mld *mld, int link_id,
 	/* Depends on rx_status->nss being configured properly, call this at
 	 * bottom of this method.
 	 */
-	iwl_mld_fill_signal(mld, link_id, hdr, rx_status, phy_data);
+	iwl_mld_fill_signal(mld, link_id, hdr, rx_status, phy_data, sta,
+			    link_sta, is_beacon, my_beacon);
 }
 
 /* iwl_mld_create_skb adds the rxb to a new skb */
@@ -1825,19 +1844,22 @@ static void iwl_mld_update_last_rx_timestamp(struct iwl_mld *mld, u8 baid)
 
 /* Processes received packets for a station.
  * Sets *drop to true if the packet should be dropped.
+ * Assigns rv_link_sta if found (NULL otherwise)
  * Returns the station if found, or NULL otherwise.
  */
 static struct ieee80211_sta *
 iwl_mld_rx_with_sta(struct iwl_mld *mld, struct ieee80211_hdr *hdr,
 		    struct sk_buff *skb,
 		    const struct iwl_rx_mpdu_desc *mpdu_desc,
-		    const struct iwl_rx_packet *pkt, int queue, bool *drop)
+		    const struct iwl_rx_packet *pkt, int queue, bool *drop,
+		    struct ieee80211_link_sta **rv_link_sta)
 {
 	struct ieee80211_sta *sta = NULL;
-	struct ieee80211_link_sta *link_sta = NULL;
+	struct ieee80211_link_sta *link_sta;
 	struct ieee80211_rx_status *rx_status;
 	u8 baid;
 
+	*rv_link_sta = NULL;
 	if (mpdu_desc->status & cpu_to_le32(IWL_RX_MPDU_STATUS_SRC_STA_FOUND)) {
 		u8 sta_id = le32_get_bits(mpdu_desc->status,
 					  IWL_RX_MPDU_STATUS_STA_ID);
@@ -1848,8 +1870,10 @@ iwl_mld_rx_with_sta(struct iwl_mld *mld, struct ieee80211_hdr *hdr,
 			return NULL;
 
 		link_sta = rcu_dereference(mld->fw_id_to_link_sta[sta_id]);
-		if (!IS_ERR_OR_NULL(link_sta))
+		if (!IS_ERR_OR_NULL(link_sta)) {
 			sta = link_sta->sta;
+			*rv_link_sta = link_sta;
+		}
 	} else if (!is_multicast_ether_addr(hdr->addr2)) {
 		/* Passing NULL is fine since we prevent two stations with the
 		 * same address from being added.
@@ -2176,6 +2200,7 @@ void iwl_mld_rx_mpdu(struct iwl_mld *mld, struct napi_struct *napi,
 	enum iwl_mld_reorder_result reorder_res;
 	struct ieee80211_rx_status *rx_status;
 	unsigned int alloc_size = 128;
+	struct ieee80211_link_sta *link_sta;
 
 	if (unlikely(mld->fw_status.in_hw_restart))
 		return;
@@ -2228,7 +2253,7 @@ void iwl_mld_rx_mpdu(struct iwl_mld *mld, struct napi_struct *napi,
 
 	rcu_read_lock();
 
-	sta = iwl_mld_rx_with_sta(mld, hdr, skb, mpdu_desc, pkt, queue, &drop);
+	sta = iwl_mld_rx_with_sta(mld, hdr, skb, mpdu_desc, pkt, queue, &drop, &link_sta);
 	if (drop)
 		goto drop;
 
@@ -2276,7 +2301,7 @@ void iwl_mld_rx_mpdu(struct iwl_mld *mld, struct napi_struct *napi,
 	link_id = u8_get_bits(mpdu_desc->mac_phy_band,
 			      IWL_RX_MPDU_MAC_PHY_BAND_LINK_MASK);
 
-	iwl_mld_rx_fill_status(mld, link_id, hdr, skb, &phy_data, sta);
+	iwl_mld_rx_fill_status(mld, link_id, hdr, skb, &phy_data, sta, link_sta);
 
 	if (iwl_mld_rx_crypto(mld, sta, hdr, rx_status, mpdu_desc, queue,
 			      le32_to_cpu(pkt->len_n_flags), &crypto_len)) {
@@ -2497,7 +2522,7 @@ static void iwl_mld_no_data_rx(struct iwl_mld *mld,
 					 ntfy->band, ntfy->channel);
 
 	/* link ID is ignored for NULL header */
-	iwl_mld_rx_fill_status(mld, -1, NULL, skb, &phy_data, NULL);
+	iwl_mld_rx_fill_status(mld, -1, NULL, skb, &phy_data, NULL, NULL);
 
 	/* No more radiotap info should be added after this point.
 	 * Mark it as mac header for upper layers to know where
