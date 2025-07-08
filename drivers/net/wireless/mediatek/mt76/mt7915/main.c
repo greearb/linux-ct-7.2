a55af7a5c7c615d9e500f8bdea3ad09cc1765a5c
Rory Little <rory@candelatech.com>
2025-07-08 10:17:27 -0700

    wifi: mt76: mt7915: Add vif-level set_rate_override debugfs file
    This is tied as a feature to VIFs, fundamentally. Therefore, it should
    exist at a VIF level. This saves us from needing to pass a VIF to the
    file for lookup.
    
    Also fix a bug for AP VIFs, where the testmode config was not being
    correctly propogated to new stations.
    
    Signed-off-by: Rory Little <rory@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7915/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7915/main.c
@@ -914,6 +914,8 @@ int mt7915_mac_sta_add(struct mt76_dev *mdev, struct ieee80211_vif *vif,
 	if (sta->tdls)
 		set_bit(MT_WCID_FLAG_TDLS_PEER, &msta->wcid.flags);
 
+	mt76_copy_rate_overrides(&msta->test, &mvif->sta.test);
+
 	ewma_avg_signal_init(&msta->avg_ack_signal);
 
 	mt7915_mac_wtbl_update(dev, idx,
@@ -2147,6 +2149,7 @@ const struct ieee80211_ops mt7915_ops = {
 	CFG80211_TESTMODE_DUMP(mt76_testmode_dump)
 #ifdef CONFIG_MAC80211_DEBUGFS
 	.sta_add_debugfs = mt7915_sta_add_debugfs,
+	.vif_add_debugfs = mt7915_vif_add_debugfs,
 #endif
 	.set_radar_background = mt7915_set_radar_background,
 #ifdef CONFIG_NET_MEDIATEK_SOC_WED
