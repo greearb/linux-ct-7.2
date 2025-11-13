60ebbb9d0be0b174e33695e62356ee46965799df
Ben Greear <greearb@candelatech.com>
2025-11-12 17:33:57 -0800

    mt76:  Add tx path debugging.
    Including storing local mac addr in wcid so that we can log it in
    tx path.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7925/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7925/main.c
@@ -443,7 +443,7 @@ static int mt7925_mac_link_bss_add(struct mt792x_dev *dev,
 
 	mlink->wcid.idx = idx;
 	mlink->wcid.tx_info |= MT_WCID_TX_INFO_SET;
-	mt76_wcid_init(&mlink->wcid, 0);
+	mt76_wcid_init(&dev->mt76, &mlink->wcid, 0, vif->addr);
 
 	mt7925_mac_wtbl_update(dev, idx,
 			       MT_WTBL_UPDATE_ADM_COUNT_CLEAR);
@@ -929,7 +929,7 @@ static int mt7925_mac_link_sta_add(struct mt76_dev *mdev,
 		return -ENOSPC;
 
 	mconf = mt792x_vif_to_link(mvif, link_id);
-	mt76_wcid_init(&mlink->wcid, 0);
+	mt76_wcid_init(&dev->mt76, &mlink->wcid, 0, vif->addr);
 	mlink->wcid.sta = 1;
 	mlink->wcid.idx = idx;
 	mlink->wcid.tx_info |= MT_WCID_TX_INFO_SET;
