b6c555b65a08e27ff4774a393e04ea600d423a7b
Rory Little <roryl@candelatech.com>
2026-06-30 11:22:58 -0700

    wifi: mt76: Add counters for DMA map/unmap to device.
    Useful for tracking a possible memory leak here.
    
    Signed-off-by: Rory Little <roryl@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt76.h
+++ drivers/net/wireless/mediatek/mt76/mt76.h
@@ -1165,6 +1165,11 @@ struct mt76_dev {
 	};
 
 	atomic_t bus_hung;
+
+#ifdef CONFIG_CT_DBG
+	atomic64_t map_counter;
+	atomic64_t unmap_counter;
+#endif
 };
 
 /* per-phy stats.  */
@@ -2617,5 +2622,13 @@ mt76_txq_dbg(struct mt76_dev *dev, struct mt76_txq *mtxq, u32 dbg_mask, const ch
 	dev_info(dev->dev, "%s: %pV", prefix_buf, &vaf);
 	va_end(args);
 }
+
+#ifdef CONFIG_CT_DBG
+#define MT76_COUNT_DMA_MAP(dev) atomic64_inc(&(dev)->map_counter)
+#define MT76_COUNT_DMA_UNMAP(dev) atomic64_inc(&(dev)->unmap_counter)
+#else
+#define MT76_COUNT_DMA_MAP(dev)
+#define MT76_COUNT_DMA_UNMAP(dev)
+#endif
 
 #endif
