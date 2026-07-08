75fd433775553803a0abbc2ba88cc6a2b78dd8ca
Rory Little <roryl@candelatech.com>
2026-07-08 14:56:44 -0700

    wifi: mac80211: Do not allocate link_sinfo unless link is valid
    Later checks will assume that an allocated link has been initialized.
    Placing the link_sinfo initialization after the valid check here causes
    code to do a naive NULL check and get caught on later uninitialized
    pointers.
    
    CT-TODO: Squash into a6363dd85f9e ("wifi: ethtool to support reporting up to 4 links.")
    Signed-off-by: Rory Little <roryl@candelatech.com>

--- net/mac80211/sta_info.c
+++ net/mac80211/sta_info.c
@@ -3539,8 +3539,6 @@ void sta_set_sinfo(struct sta_info *sta, struct station_info *sinfo,
 						     sta->link[link_id]);
 			link = wiphy_dereference(sdata->local->hw.wiphy,
 						 sdata->link[link_id]);
-			sinfo->links[link_id] = kzalloc(sizeof(*sinfo->links[0]),
-							GFP_KERNEL);
 
 			if (!link_sta || !lsi || !link) {
 				sinfo->valid_links &= ~BIT(link_id);
