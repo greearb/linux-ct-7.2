greearb/linux-ct-6.11

mt7996: Store default_sku before setting txpower.
Helps with debugging txpower settings, and also may be used
for adjusting offered power in the future.

Signed-off-by: Ben Greear <greearb@candelatech.com>

drivers/net/wireless/mediatek/mt76/mt7996/debugfs.c
@@ -1092,6 +1092,8 @@ mt7996_get_txpower_info(struct file *file, char __user *user_buf,
 	struct mt7996_phy *phy = file->private_data;
 	struct mt7996_mcu_txpower_event *event;
 	struct txpower_basic_info *basic_info;
+	struct mt76_phy *mphy = phy->mt76;
+	struct ieee80211_hw *hw = mphy->hw;
 	static const size_t size = 2048;
 	int len = 0;
 	ssize_t ret;
@@ -1148,6 +1150,16 @@ mt7996_get_txpower_info(struct file *file, char __user *user_buf,
 	len += scnprintf(buf + len, size - len,
 			 "    Thermal Compensation Value: %d\n",
 			 basic_info->thermal_compensate_value);
+
+	len += scnprintf(buf + len, size - len,
+			 "    PHY Power Bound: %d\n",
+			 mt7996_get_power_bound(phy, hw->conf.power_level));
+	len += scnprintf(buf + len, size - len,
+			 "    HW Conf Power Level: %d\n",
+			 hw->conf.power_level);
+	len += scnprintf(buf + len, size - len,
+			 "    Per-Chain TX-Power Cur: %d\n",
+			 mphy->txpower_cur);
 
 	ret = simple_read_from_buffer(user_buf, count, ppos, buf, len);
 

greearb/linux-ct-7.2

drivers/net/wireless/mediatek/mt76/mt7996/debugfs.c
@@ -1214,6 +1214,8 @@ mt7996_get_txpower_info(struct file *file, char __user *user_buf,
 	struct mt7996_phy *phy = &dev->phy;
 	struct mt7996_mcu_txpower_event *event;
 	struct txpower_basic_info *basic_info;
+	struct mt76_phy *mphy = phy->mt76;
+	struct ieee80211_hw *hw = mphy->hw;
 	static const size_t size = 2048;
 	int len = 0;
 	ssize_t ret;
@@ -1270,6 +1272,16 @@ mt7996_get_txpower_info(struct file *file, char __user *user_buf,
 	len += scnprintf(buf + len, size - len,
 			 "    Thermal Compensation Value: %d\n",
 			 basic_info->thermal_compensate_value);
+
+	len += scnprintf(buf + len, size - len,
+			 "    PHY Power Bound: %d\n",
+			 mt76_get_power_bound(mphy, hw->conf.power_level));
+	len += scnprintf(buf + len, size - len,
+			 "    HW Conf Power Level: %d\n",
+			 hw->conf.power_level);
+	len += scnprintf(buf + len, size - len,
+			 "    Per-Chain TX-Power Cur: %d\n",
+			 mphy->txpower_cur);
 
 	ret = simple_read_from_buffer(user_buf, count, ppos, buf, len);
 