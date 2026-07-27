greearb/linux-ct-6.11

mt76: mt7996: Support loading per-bus rf.bin file
So that we can have more than one radio per system and load
proper rf.bin file for each individual radio.

Signed-off-by: Ben Greear <greearb@candelatech.com>

drivers/net/wireless/mediatek/mt76/mt7996/eeprom.c
@@ -84,6 +84,8 @@ mt7996_eeprom_load_default(struct mt7996_dev *dev)
 	int ret;
 
 	ret = request_firmware(&fw, mt7996_eeprom_name(dev), dev->mt76.dev);
+	dev_warn(dev->mt76.dev, "eeprom-load-default, request-fw ret: %d  eeprom-name: %s\n",
+		 ret, mt7996_eeprom_name(dev));
 	if (ret)
 		return ret;
 
@@ -111,6 +113,7 @@ int mt7996_eeprom_check_fw_mode(struct mt7996_dev *dev)
 
 	/* load eeprom in flash or bin file mode to determine fw mode */
 	ret = mt76_eeprom_init(&dev->mt76, MT7996_EEPROM_SIZE);
+	dev_warn(dev->mt76.dev, "eeprom-check-fw-mode, eeprom-init ret: %d\n", ret);
 	if (ret < 0) {
 		mtk_dbg(&dev->mt76, CFG, "eeprom-init had error: %d\n", ret);
 		return ret;
@@ -135,6 +138,9 @@ static int mt7996_eeprom_load(struct mt7996_dev *dev)
 	u32 block_num, i;
 	u32 eeprom_blk_size = MT7996_EEPROM_BLOCK_SIZE;
 
+	dev_warn(dev->mt76.dev, "mt7996-eeprom-load, flash-mode: %d\n",
+		 dev->flash_mode);
+
 	/* flash or bin file mode eeprom is loaded before mcu init */
 	if (!dev->flash_mode) {
 		ret = mt7996_mcu_get_eeprom_free_block(dev, &free_block_num);
@@ -153,6 +159,7 @@ static int mt7996_eeprom_load(struct mt7996_dev *dev)
 			if (ret < 0 && ret != -EINVAL)
 				return ret;
 		}
+		dev_warn(dev->mt76.dev, "mt7996-eeprom-load, not flash mode, read from NIC's efuse\n");
 	}
 
 	return mt7996_check_eeprom(dev);

greearb/linux-ct-7.2

drivers/net/wireless/mediatek/mt76/mt7996/eeprom.c
@@ -141,6 +141,8 @@ mt7996_eeprom_check_or_use_default(struct mt7996_dev *dev, bool use_default)
 	int ret;
 
 	ret = request_firmware(&fw, mt7996_eeprom_name(dev), dev->mt76.dev);
+	dev_warn(dev->mt76.dev, "eeprom-load-default, request-fw ret: %d  eeprom-name: %s\n",
+		 ret, mt7996_eeprom_name(dev));
 	if (ret)
 		return ret;
 
@@ -172,10 +174,14 @@ static int mt7996_eeprom_load(struct mt7996_dev *dev)
 	mtk_dbg(&dev->mt76, CFG, "attemping eeprom-init.\n");
 
 	ret = mt76_eeprom_init(&dev->mt76, MT7996_EEPROM_SIZE);
+	dev_warn(dev->mt76.dev, "eeprom-check-fw-mode, eeprom-init ret: %d\n", ret);
 	if (ret < 0) {
 		mtk_dbg(&dev->mt76, CFG, "eeprom-init had error: %d\n", ret);
 		return ret;
 	}
+
+	dev_warn(dev->mt76.dev, "mt7996-eeprom-load, eeprom-mode: %d\n",
+		 dev->eeprom_mode);
 
 	if (ret && !mt7996_check_eeprom(dev)) {
 		dev->eeprom_mode = EEPROM_MODE_FLASH;
@@ -226,6 +232,7 @@ static int mt7996_eeprom_load(struct mt7996_dev *dev)
 			goto out;
 		}
 	}
+	dev_warn(dev->mt76.dev, "mt7996-eeprom-load, not flash mode, read from NIC's efuse\n");
 
 out:
 	return mt7996_eeprom_check_or_use_default(dev, use_default);