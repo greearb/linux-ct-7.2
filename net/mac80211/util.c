greearb/linux-ct-5.15

mac80211: More OFDMA disabling logic.
Clear our more CAP flags that I had missed in first iterations.

Signed-off-by: Ben Greear <greearb@candelatech.com>

net/mac80211/util.c
@@ -3105,12 +3105,21 @@ u8 *ieee80211_ie_build_he_cap(struct ieee80211_sub_if_data *sdata, u8 *pos,
 	if (ifmgd->flags & IEEE80211_STA_DISABLE_OFDMA) {
 		struct ieee80211_he_cap_elem *hec;
 		hec = (struct ieee80211_he_cap_elem *)(pos);
+		hec->mac_cap_info[3] &= ~IEEE80211_HE_MAC_CAP3_OMI_CONTROL;
 		hec->mac_cap_info[3] &= ~IEEE80211_HE_MAC_CAP3_OFDMA_RA;
 		hec->mac_cap_info[5] &= ~IEEE80211_HE_MAC_CAP5_HT_VHT_TRIG_FRAME_RX;
+		hec->phy_cap_info[0] &= ~IEEE80211_HE_PHY_CAP0_CHANNEL_WIDTH_SET_RU_MAPPING_IN_2G;
+		hec->phy_cap_info[0] &= ~IEEE80211_HE_PHY_CAP0_CHANNEL_WIDTH_SET_RU_MAPPING_IN_5G;
 		hec->phy_cap_info[3] &= ~IEEE80211_HE_PHY_CAP3_RX_PARTIAL_BW_SU_IN_20MHZ_MU;
 		hec->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_SU_BEAMFORMING_FB;
 		hec->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_MU_BEAMFORMING_PARTIAL_BW_FB;
 		hec->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_CQI_FB;
+		hec->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_PARTIAL_BW_EXT_RANGE;
+		hec->phy_cap_info[9] &= ~IEEE80211_HE_PHY_CAP9_LONGER_THAN_16_SIGB_OFDM_SYM;
+		hec->phy_cap_info[9] &= ~IEEE80211_HE_PHY_CAP9_TX_1024_QAM_LESS_THAN_242_TONE_RU;
+		hec->phy_cap_info[9] &= ~IEEE80211_HE_PHY_CAP9_RX_1024_QAM_LESS_THAN_242_TONE_RU;
+		hec->phy_cap_info[9] &= ~IEEE80211_HE_PHY_CAP9_RX_FULL_BW_SU_USING_MU_WITH_COMP_SIGB;
+		hec->phy_cap_info[9] &= ~IEEE80211_HE_PHY_CAP9_RX_FULL_BW_SU_USING_MU_WITH_NON_COMP_SIGB;
 
 		/* Disable MU related OFDMA stuff too */
 		hec->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_PARTIAL_BANDWIDTH_DL_MUMIMO;

greearb/linux-ct-7.2

net/mac80211/util.c
@@ -2732,12 +2732,22 @@ ieee80211_get_adjusted_he_cap(const struct ieee80211_conn_settings *conn,
 
 	if (conn->conn_flags & IEEE80211_CONN_DISABLE_OFDMA) {
 		pr_info("adjust-he-cap, disabling OFDMA.");
+		elem->mac_cap_info[3] &= ~IEEE80211_HE_MAC_CAP3_OMI_CONTROL;
 		elem->mac_cap_info[3] &= ~IEEE80211_HE_MAC_CAP3_OFDMA_RA;
 		elem->mac_cap_info[5] &= ~IEEE80211_HE_MAC_CAP5_HT_VHT_TRIG_FRAME_RX;
+		elem->phy_cap_info[0] &= ~IEEE80211_HE_PHY_CAP0_CHANNEL_WIDTH_SET_RU_MAPPING_IN_2G;
+		elem->phy_cap_info[0] &= ~IEEE80211_HE_PHY_CAP0_CHANNEL_WIDTH_SET_RU_MAPPING_IN_5G;
 		elem->phy_cap_info[3] &= ~IEEE80211_HE_PHY_CAP3_RX_PARTIAL_BW_SU_IN_20MHZ_MU;
 		elem->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_SU_BEAMFORMING_FB;
 		elem->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_MU_BEAMFORMING_PARTIAL_BW_FB;
 		elem->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_CQI_FB;
+		elem->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_PARTIAL_BW_EXT_RANGE;
+		elem->phy_cap_info[9] &= ~IEEE80211_HE_PHY_CAP9_LONGER_THAN_16_SIGB_OFDM_SYM;
+		elem->phy_cap_info[9] &= ~IEEE80211_HE_PHY_CAP9_TX_1024_QAM_LESS_THAN_242_TONE_RU;
+		elem->phy_cap_info[9] &= ~IEEE80211_HE_PHY_CAP9_RX_1024_QAM_LESS_THAN_242_TONE_RU;
+		elem->phy_cap_info[9] &= ~IEEE80211_HE_PHY_CAP9_RX_FULL_BW_SU_USING_MU_WITH_COMP_SIGB;
+		elem->phy_cap_info[9] &= ~IEEE80211_HE_PHY_CAP9_RX_FULL_BW_SU_USING_MU_WITH_NON_COMP_SIGB;
+
 
 		/* Disable MU related OFDMA stuff too */
 		elem->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_PARTIAL_BANDWIDTH_DL_MUMIMO;